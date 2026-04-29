78cab7aa64c45a9d88d15fc547da4008144ed7eb
Wagner Paulo <wagstalos@hotmail.com>
2026-04-29 11:29:39 -0300

    feat: implement smooth scroll functionality and add responsive hero section styles

--- js/scripts/scroll.js
+++ js/scripts/scroll.js
@@ -1,23 +1,23 @@
-// function scrollTarget() {
-//   var links = document.querySelectorAll('a[href^="#"]');
+function scrollTarget() {
+  var links = document.querySelectorAll('a[href^="#"]');
 
-//   links.forEach(function (link) {
-//     link.addEventListener("click", function (event) {
-//       event.preventDefault();
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
 
-//       var targetId = link.getAttribute("href").substring(1);
-//       var target = document.getElementById(targetId);
+      var targetId = link.getAttribute("href").substring(1);
+      var target = document.getElementById(targetId);
 
-//       if (target) {
-//         var targetOffset = target.getBoundingClientRect().top + window.scrollY;
+      if (target) {
+        var targetOffset = target.getBoundingClientRect().top + window.scrollY;
 
-//         window.scrollTo({
-//           top: targetOffset,
-//           behavior: "smooth",
-//         });
-//       }
-//     });
-//   });
-// }
+        window.scrollTo({
+          top: targetOffset,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+}
 
-// scrollTarget();
+scrollTarget();
